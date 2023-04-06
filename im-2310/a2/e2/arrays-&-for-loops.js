4bf5a86f27846d379d0cf0f1bccf1ea5b649bcb0
joelsymons <127356038+joelsymons@users.noreply.github.com>
2023-04-06 12:10:48 +1000

    Add files via upload

---
+++ im-2310/a2/e2/arrays-&-for-loops.js
@@ -0,0 +1,20 @@
+var fruit1 = "banana";
+var fruit2 = "pineapple";
+var fruit3 = "apple";
+var fruit4 = "peach";
+var fruit5 = "orange";
+
+var fruitArray = ["banana", "pineapple", "apple", "peach", "orange"];
+
+var mixedArray = [42, "pineapple", true, [3, 6, "nine"], "orange"];
+
+console.log("I'm going to help test the array");
+
+console.log(fruitArray);
+
+fruitArray[1]  = "tomato";
+
+console.log(fruitArray);
+
+console.log(mixedArray[3]);
+
